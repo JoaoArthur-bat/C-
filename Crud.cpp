1b38d29712a5c09f355a5ffcede2ffaa6b04ce04
Joao Arthur de Souza Venancio <joaoasv@PVAI.LAN>
2025-08-07 10:48:54 -0300

    Adicionando arquivo Crud.cpp

---
+++ Crud.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	create;
+
+	return 0;
+}
+
+void create(){
+	char nome;
+	int	idade;
+	cout << "qual o seu nome " << endl;
+	cin >> nome;
+}
+
+void read(){
+
+}
+
+void update(){
+
+}
+
+void Delete(){ 
+
+}
